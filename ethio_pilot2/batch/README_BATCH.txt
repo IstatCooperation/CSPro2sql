856a64eed1f65d0be69b456f99f8edcaf8cafba5
UTENTE <UTENTE@NB75392>
2019-08-15 10:07:33 +0200

    Modified main page (now it contains the map) Refactoring and minor bug fixes

--- ethio_pilot2/batch/README_BATCH.txt
+++
@@ -1,4 +0,0 @@
-This folder contains to batch files, that can be scheduled during the survey:
-
-batch_load.bat:   executes cspro2sql 'loader' engine
-batch_update.bat: executes cspro2sql 'update' engine
--- ethio_pilot2/batch/batch_load.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-echo  %date%-%time%
-ECHO Starting batch_load execution at %date% – %time% >> C:\Users\UTENTE\Desktop\CsPro2Sql\ethio_pilot2\batch\log\batch_load.log
-java -cp C:\Users\UTENTE\Desktop\CSPro2sql\lib\cspro2sql.jar;C:\Users\UTENTE\Desktop\CSPro2sql\lib\commons-cli-1.3.1.jar;C:\Users\UTENTE\Desktop\CSPro2sql\lib\mysql-connector-java-8.0.16.jar cspro2sql.Main %* -e loader -p C:\Users\UTENTE\Desktop\CsPro2Sql\ethio_pilot2\ethio_pilot2.properties -f >> C:\Users\UTENTE\Desktop\CsPro2Sql\ethio_pilot2\batch\log\batch_load.log 2>>&1
